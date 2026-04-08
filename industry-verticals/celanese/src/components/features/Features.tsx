5e6720e52f61ed8f4c308485611595699f8b33da
Kaitlyn Folsom <kaitlynmfolsom@gmail.com>
2026-04-07 20:02:02 -0400

    component update and content pull

--- industry-verticals/celanese/src/components/features/Features.tsx
+++ industry-verticals/celanese/src/components/features/Features.tsx
@@ -53,33 +53,53 @@ export const Default = (props: FeaturesProps) => {
   // results of the graphql
   const results = props.fields.data.datasource.children.results;
   const featureSectionTitle = props.fields.data.datasource.title;
+  const numOfItems = results.length;
 
   return (
     <FeatureWrapper props={props}>
-      <div className="container grid grid-cols-1 py-16 md:py-20 lg:grid-cols-[1fr_2fr] lg:gap-12">
+      <div className="container grid grid-cols-1 py-16 md:py-20 lg:gap-12">
         <div className="mb-16 lg:mb-0">
-          <h2 className="inline-block max-w-md font-medium max-lg:text-3xl lg:text-4xl">
+          <h2 className="inline-block max-w-md font-medium max-lg:text-3xl lg:max-w-lg lg:text-4xl">
             <Text field={featureSectionTitle.jsonValue} />
           </h2>
         </div>
-        <div className="grid grid-cols-1 gap-12 md:grid-cols-2 md:gap-10 lg:grid-cols-3">
+        <div
+          className={`${numOfItems === 2 ? 'lg:grid-cols-2' : 'lg:grid-cols-3'} grid grid-cols-1 gap-12 md:grid-cols-2 md:gap-10`}
+        >
           {results.map((item, index) => {
             const title = item.featureTitle.jsonValue;
             const description = item.featureDescription.jsonValue;
             const link = item.featureLink.jsonValue;
+
             return (
-              <div
-                className="border-border bg-background flex flex-col border p-6 transition-shadow"
-                key={index}
-              >
+              <div className="flex flex-col bg-[#eee] p-6 transition-shadow" key={index}>
                 <div className="text-foreground mb-4 text-xl font-semibold">
                   <Text field={title} />
                 </div>
                 <div className="text-foreground-light mb-4 flex-auto leading-relaxed">
                   <Text field={description} />
                 </div>
-                <div className="mt-auto">
-                  <Link field={link} className="arrow-btn" />
+                <div className="mt-auto flex items-center">
+                  <>
+                    <Link
+                      field={link}
+                      className="mr-3 flex items-center text-xl font-bold text-[#b44000]"
+                    />
+                    <div className="flex items-center gap-2 rounded-full border border-white bg-[#b44000] p-2 text-white">
+                      <svg
+                        stroke="currentColor"
+                        fill="currentColor"
+                        stroke-width="0"
+                        viewBox="0 0 24 24"
+                        height="1em"
+                        width="1em"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path fill="none" d="M0 0h24v24H0z"></path>
+                        <path d="M12 4l-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8z"></path>
+                      </svg>
+                    </div>
+                  </>
                 </div>
               </div>
             );
@@ -116,15 +136,17 @@ export const ThreeColGridCentered = (props: FeaturesProps) => {
 
   return (
     <FeatureWrapper props={props}>
-      <div className="container flex flex-col flex-wrap justify-evenly gap-20 md:flex-row lg:gap-20">
+      <div className="container grid grid-cols-1 justify-evenly gap-6 py-8 md:grid-cols-2 lg:grid-cols-3 lg:gap-16 lg:py-24">
         {results.map((item, index) => {
+          console.log(item);
           const title = item.featureTitle.jsonValue;
           const description = item.featureDescription.jsonValue;
           const image = item.featureImage.jsonValue;
+          const link = item.featureLink.jsonValue;
           return (
-            <div className="flex flex-col items-center justify-start 2xl:w-80" key={index}>
+            <div className="flex flex-col items-center justify-center bg-[#fff] p-4" key={index}>
               {/* Image */}
-              <div className="bg-teal text-background mb-7 flex h-20 w-20 items-center justify-center rounded-full">
+              <div className="text-background justify-centerl mb-7 flex h-20 w-20 items-center">
                 <Image field={image} />
               </div>
               {/* Title and Description */}
@@ -134,6 +156,12 @@ export const ThreeColGridCentered = (props: FeaturesProps) => {
                 </div>
                 <div className="text-foreground-light text-center">
                   <Text field={description} />
+                </div>
+                <div className="mt-4">
+                  <Link
+                    field={link}
+                    className="text-primary border-primary mt-3 block w-full border-1 p-3 px-8"
+                  />
                 </div>
               </div>
             </div>
